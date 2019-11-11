f275056ecf6be09f02b187ec905466631652ad1f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-11 10:23:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12
       puts "Happy Birthday #{kids_name}! you are now #{age} years old!"
     else
-      puts "You are too old for this."
+      return puts "You are too old for this."
     end
   end
 end
